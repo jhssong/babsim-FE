ade46969807bccb49787f4b4bf1e172beb669102
pobi <researcherdragon@gmail.com>
2024-07-19 16:00:02 +0900

    chore: Add global theme

---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+import { orange, grey } from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: orange[800],
+        },
+        secondary: {
+            main: orange[300],
+        },
+        seperator: {
+            main: orange[100],
+        },
+        subbackground: {
+            main: grey[200],
+        },
+    },
+});
+
+export default theme;
