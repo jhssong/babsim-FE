b544a8c0467a3f624c39cacfc7f4fc359feb0638
jhssong02 <jhssong02@gmail.com>
2024-07-20 01:02:18 +0900

    chore: Add allergy list with images

---
+++ src/assets/constants/allergyList.js
@@ -0,0 +1,97 @@
+const allergyList = [
+  {
+    id: 1,
+    name: '알류',
+    imgURL: 'ef4594f7-1dbd-43c9-8397-f361b2a9ecad',
+  },
+  {
+    id: 2,
+    name: '우유',
+    imgURL: '6f27b9f5-d6c9-4e9c-bc82-86f60a8c90e1',
+  },
+  {
+    id: 3,
+    name: '메밀',
+    imgURL: 'fe3ee6ac-fff8-4e58-8c4f-ac0e80da0f2a',
+  },
+  {
+    id: 4,
+    name: '땅콩',
+    imgURL: '8700e1a6-f61f-46f0-b4ce-7e92c72ba106',
+  },
+  {
+    id: 5,
+    name: '대두',
+    imgURL: '5e076acb-4477-4710-8453-c9ec0578d144',
+  },
+  {
+    id: 6,
+    name: '밀',
+    imgURL: 'a7a40470-1a21-4d5c-a725-a8d65f701b49',
+  },
+  {
+    id: 7,
+    name: '잣',
+    imgURL: 'ad1c75da-7e96-4419-981f-8b23440a5974',
+  },
+  {
+    id: 8,
+    name: '호두',
+    imgURL: '44693f99-0c1a-4e09-bb0a-97ebfa0b8f19',
+  },
+  {
+    id: 9,
+    name: '게',
+    imgURL: '761563f0-8f2f-4963-9f4e-1d2cccdda823',
+  },
+  {
+    id: 10,
+    name: '새우',
+    imgURL: '6ce2270c-2cb2-4044-967e-cc557962c7d6',
+  },
+  {
+    id: 11,
+    name: '오징어',
+    imgURL: '8bcb3d72-cdea-4190-8fa3-8e739434faaa',
+  },
+  {
+    id: 12,
+    name: '고등어',
+    imgURL: 'b32e39ec-7b40-486a-9c7c-f5acee34477a',
+  },
+  {
+    id: 13,
+    name: '조개류',
+    imgURL: '95ba3140-197b-4673-9a9a-318f631aed61',
+  },
+  {
+    id: 14,
+    name: '복숭아',
+    imgURL: 'ac166b3f-0d6e-4144-acf3-6bb9d21e98f1',
+  },
+  {
+    id: 15,
+    name: '토마토',
+    imgURL: 'e7705955-7e95-4cec-9d7d-425675d81705',
+  },
+  {
+    id: 16,
+    name: '닭고기',
+    imgURL: 'ea2cdcd6-4a14-4eba-a156-8750c3243249',
+  },
+  {
+    id: 17,
+    name: '돼지고기',
+    imgURL: '98ac9cbd-1ceb-40b1-8a7c-7670de9a96fe',
+  },
+  {
+    id: 18,
+    name: '쇠고기',
+    imgURL: '994e0869-0570-423a-a363-bc161fa708ae',
+  },
+  {
+    id: 19,
+    name: '아황산류',
+    imgURL: '608728a5-aff8-43e3-ba7c-9491fdf53318',
+  },
+];
