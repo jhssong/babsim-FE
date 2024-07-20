274fa02537c988211a33362a2de9faf903a2da51
pobi <researcherdragon@gmail.com>
2024-07-21 00:16:09 +0900

    temp

--- src/pages/Market/Market.jsx
+++ src/pages/Market/Market.jsx
@@ -1,5 +1,33 @@
+import styled from '@emotion/styled';
+
+import { AppBarWithLogo } from '../../components/AppBar';
+import NavBar from '../../components/NavBar';
+import { useRecoilValue } from 'recoil';
+import { loginState } from '../../recoil/atoms';
+
+const Container = styled.div`
+  display: flex;
+  padding: 1rem 0rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+  flex: 1 0 0;
+  align-self: stretch;
+  overflow-y: auto;
+  height: calc(100vh - 7rem);
+`;
+
 const Market = () => {
-  return <></>;
+  const isLoggined = useRecoilValue(loginState).isLoggedIn;
+
+  return (
+    <>
+      <AppBarWithLogo />
+      <Container></Container>
+
+      <NavBar page="market" />
+    </>
+  );
 };
 
 export default Market;
