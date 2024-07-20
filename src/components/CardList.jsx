61952832a441067f9a3c5e7a007d86c3b3f5056d
pobi <researcherdragon@gmail.com>
2024-07-20 22:27:55 +0900

    feat: Add Card and CardList

---
+++ src/components/CardList.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+import { Typography } from '@mui/material';
+
+const CardListContainer = styled.div`
+  display: flex;
+  padding: 0.5rem var(--none, 0rem);
+  justify-content: space-between;
+  align-items: flex-start;
+  align-content: center;
+  gap: 1rem 0.5rem;
+  align-self: stretch;
+  flex-wrap: wrap;
+`;
+
+const CardList = ({ children }) => {
+  return <CardListContainer>{children}</CardListContainer>;
+};
+
+export default CardList;
