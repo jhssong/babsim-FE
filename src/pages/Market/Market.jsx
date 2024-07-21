69f1795ad8ca7ac639918412ec99cb2c96e6e056
pobi <researcherdragon@gmail.com>
2024-07-21 17:15:58 +0900

    temp

--- src/pages/Market/Market.jsx
+++ src/pages/Market/Market.jsx
@@ -6,6 +6,8 @@ import NavBar from '../../components/NavBar';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { RollCardList } from '../../components/CardList';
+import { VCard } from '../../components/Card';
 
 const Container = styled.div`
   display: flex;
@@ -19,16 +21,68 @@ const Container = styled.div`
 `;
 
 const Market = () => {
-  const images = [
+  const bannerImages = [
     { url: '/assets/banners/chip.webp', alt: '감자칩 요리 공모전' },
     { url: '/assets/banners/gyoza.webp', alt: '왕교자 요리 공보전' },
   ];
+  const nftDates = {
+    list: [
+      {
+        id: 'NFT001',
+        img: 'https://example.com/nft001.jpg',
+        recipeID: 'recipe001',
+        name: 'Delicious Spaghetti NFT',
+        price: '10.00 ETH',
+      },
+      {
+        id: 'NFT002',
+        img: 'https://example.com/nft002.jpg',
+        recipeID: 'recipe002',
+        name: 'Tasty Burger NFT',
+        price: '12.50 ETH',
+      },
+      {
+        id: 'NFT003',
+        img: 'https://example.com/nft003.jpg',
+        recipeID: 'recipe003',
+        name: 'Yummy Sushi NFT',
+        price: '15.00 ETH',
+      },
+      {
+        id: 'NFT004',
+        img: 'https://example.com/nft004.jpg',
+        recipeID: 'recipe004',
+        name: 'Hearty Salad NFT',
+        price: '8.75 ETH',
+      },
+      {
+        id: 'NFT005',
+        img: 'https://example.com/nft005.jpg',
+        recipeID: 'recipe005',
+        name: 'Sweet Dessert NFT',
+        price: '11.25 ETH',
+      },
+    ],
+  };
 
   return (
     <>
       <AppBarWithLogo />
       <Container>
-        <Banner images={images} />
+        <Banner images={bannerImages} />
+        <RollCardList title="NFT 상품">
+          {nftDates.list.map((nft, index) => (
+            <VCard
+              key={nft.id}
+              type="nft"
+              product={nft}
+              index={index}
+              style={{
+                marginRight: index === nftDates.list.length - 1 ? '1rem' : '0',
+              }}
+            />
+          ))}
+        </RollCardList>
       </Container>
 
       <NavBar page="market" />
