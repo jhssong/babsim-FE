95f793f020702dbffdc827539077014dac78b23f
pobi <researcherdragon@gmail.com>
2024-07-21 23:59:18 +0900

    temp

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,5 +1,36 @@
+import styled from '@emotion/styled';
+
+import { AppBarWithLogo } from '../../components/AppBar';
+import NavBar from '../../components/NavBar';
+import { useRecoilValue } from 'recoil';
+import { loginState } from '../../recoil/atoms';
+
+const Container = styled.div`
+  display: flex;
+  padding: 1rem 0rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+  flex: 1 0 0;
+  align-self: stretch;
+  overflow-y: auto;
+  overflow-x: hidden;
+  height: calc(100vh - 7rem);
+`;
+
 const Recipe = () => {
-  return <div>Recipe</div>;
+  const isLoggined = useRecoilValue(loginState).isLoggedIn;
+
+  return (
+    <>
+      <AppBarWithLogo />
+      <Container></Container>
+
+      <NavBar page="recipe" />
+    </>
+  );
 };
+
+const Categoty = () => {};
 
 export default Recipe;
