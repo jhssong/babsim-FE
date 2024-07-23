0cfe56e5a75927d57dfeab896795fd21e642a407
jhssong <jhssong02@gmail.com>
2024-07-23 11:52:42 +0900

    chore: Extract Login screens

---
+++ src/pages/Login/UserAllergySetting.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react';
+import allergyList from '../../assets/constants/allergyList';
+import createMember from '../../apis/Login/createMember';
+import styled from '@emotion/styled';
+import { AppBarWithTitle } from '../../components/AppBar';
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+
+const AllergyReqWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  flex-shrink: 0;
+  padding: 2.5rem 1rem;
+`;
+
+const WidthFill = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Divider40 = styled.div`
+  height: 2.5rem;
+`;
+
+const UserAllergySetting = ({ loginData, setLoginLevel }) => {
+  const [allergies, setAllergies] = useState(allergyList);
+
+  const handleToggle = (index) => () => {
+    const newAllergies = [...allergies];
+
+    if (newAllergies[index].checked == 1) newAllergies[index].checked = 0;
+    else newAllergies[index].checked = 1;
+
+    setAllergies(newAllergies);
+  };
+
+  async function handleClick() {
+    const finalLoginData = { ...loginData };
+    finalLoginData['allergy'] = getAllergyList();
+    console.log(finalLoginData);
+    const loggedData = await createMember(finalLoginData);
+  }
+
+  function getAllergyList() {
+    const allergy = [];
+    allergies.map((item) => {
+      if (item.checked == 1) allergy.push(item.id);
+    });
+    return allergy;
+  }
+
+  return (
+    <>
+      <AppBarWithTitle
+        title={null}
+        rightIcon={null}
+        onBackBtnClick={() => setLoginLevel(1)}
+        set={null}
+      />
+      <AllergyReqWrapper>
+        <WidthFill>
+          <Typography variant="h6">알러지 정보를 선택해주세요.</Typography>
+        </WidthFill>
+        <Divider40 />
+        <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          {allergies.map((item, index) => {
+            const labelId = `checkbox-list-secondary-label-${item.id}`;
+            return (
+              <ListItem
+                key={item.id}
+                secondaryAction={
+                  <Checkbox
+                    edge="end"
+                    onChange={handleToggle(index)}
+                    checked={item.checked == 1}
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                }
+                disablePadding>
+                <ListItemButton sx={{ padding: '0.5rem 0' }}>
+                  <ListItemAvatar>
+                    <Avatar alt={item.name} src={`/assets/images/allergies/${item.imgURL}.png`} />
+                  </ListItemAvatar>
+                  <ListItemText id={labelId} primary={item.name} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+        <Divider40 />
+        <WidthFill>
+          <Button variant="contained" color="primary" sx={{ width: '100%' }} onClick={handleClick}>
+            <Typography variant="button" fontWeight="bold">
+              다음
+            </Typography>
+          </Button>
+        </WidthFill>
+        <Divider40 />
+      </AllergyReqWrapper>
+    </>
+  );
+};
+
+export default UserAllergySetting;
---
+++ src/pages/Login/UserInfoSetting.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import { AppBarWithTitle } from '../../components/AppBar';
+import styled from '@emotion/styled';
+import { Avatar, Button, MenuItem, TextField, Typography } from '@mui/material';
+
+const InfoReqWrapper = styled.div`
+  display: flex;
+  height: calc(100vh - 3rem);
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  flex-shrink: 0;
+  padding: 2.5rem 1rem;
+`;
+
+const WidthFill = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Divider40 = styled.div`
+  height: 2.5rem;
+`;
+
+const UserInfoSetting = ({ loginData, setLoginData, setLoginLevel }) => {
+  const [name, setName] = useState(loginData.name);
+  const [nameError, setNameError] = useState(false);
+  const [job, setJob] = useState(loginData.job ?? '');
+  const [jobError, setJobError] = useState(false);
+
+  const handleName = (event) => {
+    setNameError(event.target.value == null);
+    setName(event.target.value);
+  };
+
+  const handleJob = (event) => {
+    setJobError(event.target.value == '');
+    setJob(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (name == '') {
+      setNameError(true);
+      return;
+    }
+    if (typeof job != 'number') {
+      setJobError(true);
+      return;
+    }
+
+    const newLoginData = { ...loginData };
+    newLoginData['job'] = job;
+    setLoginData(newLoginData);
+    setLoginLevel(2);
+  };
+
+  return (
+    <>
+      <AppBarWithTitle
+        title={null}
+        rightIcon={null}
+        onBackBtnClick={() => setLoginLevel(0)}
+        set={null}
+      />
+      <InfoReqWrapper>
+        <WidthFill>
+          <Typography variant="h6">기본적인 정보를 확인할게요.</Typography>
+        </WidthFill>
+        <Divider40 />
+        <Avatar
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+          sx={{ width: 100, height: 100 }}
+        />
+        <Divider40 />
+        <TextField
+          id="memberName"
+          label="이름"
+          sx={{ width: '100%' }}
+          onChange={handleName}
+          defaultValue={name}
+          inputProps={{ maxLength: 20 }}
+          error={nameError}
+          helperText={nameError ? '이름을 입력해주세요' : ''}
+        />
+        <Divider40 />
+        <WidthFill>
+          <TextField
+            value={job}
+            onChange={handleJob}
+            select
+            label="직업"
+            sx={{ width: '100%' }}
+            error={jobError}
+            helperText={jobError ? '직업을 선택해주세요' : ''}>
+            <MenuItem value={1}>학생</MenuItem>
+            <MenuItem value={2}>직장인</MenuItem>
+            <MenuItem value={3}>주부</MenuItem>
+          </TextField>
+        </WidthFill>
+        <Divider40 />
+        <WidthFill>
+          <Button variant="contained" color="primary" sx={{ width: '100%' }} onClick={handleClick}>
+            <Typography variant="button" fontWeight="bold">
+              다음
+            </Typography>
+          </Button>
+        </WidthFill>
+      </InfoReqWrapper>
+    </>
+  );
+};
+
+export default UserInfoSetting;
