4c7eb5594da20e753aa61ebbc1ba48ca53d0bb8b
pobi <researcherdragon@gmail.com>
2024-07-28 09:09:45 +0900

    feat: Add temp cook page

--- src/pages/Recipe/Cook.jsx
+++ src/pages/Recipe/Cook.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { AppBarWithTitle } from '../../components/AppBar';
+import { VCard } from '../../components/Card';
 
 const Cook = () => {
   return (
@@ -13,9 +14,7 @@ const Cook = () => {
         height: '100vh',
       }}>
       <AppBarWithTitle title="Cook" rightIcon="list" />
-      <Typography variant="h6" color="primary">
-        cook
-      </Typography>
+      <VCard />
     </Box>
   );
 };
