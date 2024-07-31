61223e0df64dce3e8394afb355e8ef4c9d740c22
ijh1298 <imjh0417@gmail.com>
2024-07-31 17:10:49 +0900

    feat: Add putRecipeEditAPI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,32 +20,6 @@ const Container = styled.div`
   height: calc(100vh - 7rem);
 `;
 
-/*
-레시피 조회 API
-{path} : recipeId
-@memberID (nullable) (String)
-*/
-async function getRecipeInfo(recipeId, memberId) {
-  let url = 'http://localhost:8080/api/recipes/' + recipeId;
-  // // Query Parameter를 추가 (memberID가 있는 경우에만)
-  if (memberId) {
-    const queryParams = new URLSearchParams({ memberId });
-    url += '?' + queryParams.toString();
-  }
-
-  console.log(`${url} and working`);
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (!response.ok) throw new Error('Failed to get recipe info');
-  const responseData = await response.json();
-  console.log(responseData);
-  return responseData;
-}
-
 const Home = () => {
   const isLoggined = useRecoilValue(isLoggedInState);
 
